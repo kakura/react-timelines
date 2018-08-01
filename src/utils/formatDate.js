4fd6d57c38ffda5e08f82f9246aa6d365f19a8b8
shuta kakura <kakura23@gmail.com>
2018-08-01 19:09:33 +0900

    Update formatDate.js
    日本語に変更

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,5 +1,5 @@
-const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+const monthNames = ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'];
 
-export const getMonth = date => monthNames[date.getMonth()]
+export const getMonth = date => monthNames[date.getMonth()];
 
-export const getDayMonth = date => `${date.getDate()} ${getMonth(date)}`
+export const getDayMonth = date => `${date.getDate()} ${getMonth(date)}`;
